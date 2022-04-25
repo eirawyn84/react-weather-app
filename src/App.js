8a69a911c85132b65b871e86370d8de1d4d52209
eirawyn84 <97123947+eirawyn84@users.noreply.github.com>
2022-04-25 15:09:51 +0100

    Added the gitHub repo URL

--- src/App.js
+++ src/App.js
@@ -2,5 +2,19 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Era Owen and is{" "}
+        <a
+          href="https://github.com/eirawyn84/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Opern-Sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
