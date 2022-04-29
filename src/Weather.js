74d2e375c3498241658804edbba6a915b384a4cc
eirawyn84 <97123947+eirawyn84@users.noreply.github.com>
2022-04-29 14:54:56 +0100

    Adding CSS style to app

--- src/Weather.js
+++ src/Weather.js
@@ -3,42 +3,54 @@ import "./Weather.css";
 
 export default function Weather() {
   return (
-    <div className="Weather">
-      <form>
-        <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Enter a City ..."
-              className="form-control"
-            />
+    <div>
+      <div className="Weather mt-3">
+        <form className="m-1">
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a City ..."
+                className="form-control"
+                autoFocus="on"
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100"
+              />
+            </div>
           </div>
-          <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary" />
-          </div>
-        </div>
-      </form>
+        </form>
 
-      <h1>New York</h1>
-      <div className="WeatherInfo">
-        <ul>
-          <li>Thursday 14:04</li>
-          <li>Few Clouds</li>
-        </ul>
-        <div className="row">
-          <div className="col-6">
-            <img
-              src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
-              alt="cloudy"
-            ></img>
-            <span className="Temperature">7</span>
-            <span className="Unit">°C</span>
-          </div>
-          <div className="col-6">
-            <ul>
-              <li>Humidity:45%</li>
-              <li>Wind: 9.39 km/h</li>
-            </ul>
+        <h1>New York</h1>
+        <div className="WeatherInfo">
+          <ul>
+            <li>Thursday 14:04</li>
+            <li>Few Clouds</li>
+          </ul>
+          <div className="row mt-3">
+            <div className="col-6">
+              <div className="d-flex">
+                <img
+                  src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
+                  alt="cloudy"
+                  className="float-left"
+                />
+                <div className="float-left">
+                  <span className="Temperature">7</span>
+                  <span className="Unit">°C</span>
+                </div>
+              </div>
+            </div>
+            <div className="col-6">
+              <ul>
+                <li>Humidity:45%</li>
+                <li>Wind: 9.39 km/h</li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
